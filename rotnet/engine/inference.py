75b76a880bea59b25c1eea22207470d7bc363c2d
zjkjzj <wy163zhuj@163.com>
2020-09-01 20:55:54 +0800

    perf(infer): 调用topk_accuracy计算准确率

--- rotnet/engine/inference.py
+++ rotnet/engine/inference.py
@@ -7,51 +7,37 @@
 @description: 
 """
 
-import os
-import numpy as np
 from tqdm import tqdm
 import torch
 import logging
 
+from rotnet.util.metrics import topk_accuracy
 from rotnet.data.build import build_dataloader
-from rotnet.util.logger import setup_logger
 
 
 def compute_on_dataset(model, data_loader, device):
-    results_dict = {}
-    acc_dict = {}
-    total_error_distance = 0
-    for i in range(360):
-        results_dict[str(i)] = 0
-        acc_dict[str(i)] = 0
+    cpu_device = torch.device("cpu")
+    running_acc = 0.0
 
     for batch in tqdm(data_loader):
         images, targets = batch
-        cpu_device = torch.device("cpu")
-        with torch.no_grad():
-            outputs = model(images.to(device))
-            outputs = [o.to(cpu_device) for o in outputs]
-        for target, result in zip(targets, outputs):
-            idx = str(target.item())
-            pred = torch.argmax(result).item()
+        images = images.to(device)
+        targets = targets.to(device)
 
-            results_dict[idx] += 1
-            acc_dict[idx] += (pred == target)
-            total_error_distance += abs(pred - target)
-    return results_dict, acc_dict, total_error_distance
+        with torch.no_grad():
+            outputs = model(images)
+            topk_list = topk_accuracy(outputs, targets, topk=(1,))
+
+            running_acc += topk_list[0].to(cpu_device).item()
+    return running_acc / len(data_loader)
 
 
 def inference(cfg, model, data_loader, dataset_name, device):
     dataset = data_loader.dataset
     logger = logging.getLogger(cfg.TEST.NAME)
     logger.info("Evaluating {} dataset({} images):".format(dataset_name, len(dataset)))
-    results_dict, acc_dict, total_error_distance = compute_on_dataset(model, data_loader, device)
-    # print(results_dict)
-    # print(acc_dict)
-    total_num = np.sum(list(results_dict.values()))
-    acc_num = np.sum(list(acc_dict.values()))
-    logger.info('acc rate: {:.3f}, avg error distance: {:.3f}'.format(
-        1.0 * acc_num / total_num, 1.0 * total_error_distance / total_num))
+    mean_acc = compute_on_dataset(model, data_loader, device)
+    logger.info('acc rate: {:.3f}'.format(mean_acc))
 
     for handler in logger.handlers:
         logger.removeHandler(handler)
