5a849c3891b0e77a35cff35d6d35bace46140bbe
zjkjzj <wy163zhuj@163.com>
2021-03-14 20:20:53 +0800

    perf(transforms): when train, use random borderValue; when test, use 255 value

--- rotnet/data/transforms/build.py
+++ rotnet/data/transforms/build.py
@@ -12,12 +12,15 @@ from zcls.data.transforms.build import parse_transform
 from .rotate import Rotate
 
 
-def build_target_transform():
+def build_target_transform(is_train=True):
     """
     Rotate image randomly. The test set and training set are rotated randomly
     """
-    return Rotate(random=True)
+    if is_train:
+        return Rotate(random=True)
+    else:
+        return Rotate(random=False)
 
 
 def build_transform(cfg, is_train=True):
-    return parse_transform(cfg, is_train), build_target_transform()
+    return parse_transform(cfg, is_train), build_target_transform(is_train)
