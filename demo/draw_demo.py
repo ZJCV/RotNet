861695b5b030713c091f76eaa1d5d3e36b2cb2d1
zjkjzj <wy163zhuj@163.com>
2021-03-14 20:18:42 +0800

    refactor(demo): update

--- demo/draw_demo.py
+++ demo/draw_demo.py
@@ -4,8 +4,7 @@
 @date: 2020/9/5 下午5:19
 @file: draw_demo.py
 @author: zj
-@description: 
-绘图，比较检测结果
+@description: Plot and compare the test results
 """
 
 import cv2
@@ -24,21 +23,22 @@ def draw(src_img_list, rotate_img_list, res_img_list):
     rows = len(src_img_list)
     for i in range(rows):
         img_path = src_img_list[i]
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
         plt.subplot(rows, 3, 3 * i + 1)
         plt.imshow(img, cmap='gray'), plt.axis('off')
 
         img_path = rotate_img_list[i]
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
         plt.subplot(rows, 3, 3 * i + 2)
         plt.imshow(img, cmap='gray'), plt.axis('off')
 
         img_path = res_img_list[i]
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
         plt.subplot(rows, 3, 3 * i + 3)
         plt.imshow(img, cmap='gray'), plt.axis('off')
 
-    plt.savefig('gray.png', bbox_inches='tight')
+    plt.savefig('./demo/demo.png', bbox_inches='tight')
     plt.show()
 
 
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 
     src_img_list = list()
     for img_path in rotate_img_list:
-        idx = os.path.splitext(os.path.split(img_path)[1])[0].split('_')[0]
+        idx = os.path.splitext(os.path.split(img_path)[1])[0].split('-')[0]
         src_img_path = os.path.join(src_img_dir, f'{idx}.jpg')
         src_img_list.append(src_img_path)
     print(src_img_list)
